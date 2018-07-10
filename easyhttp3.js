371b53d848dfd73af67856448b0c89ed1dc21840
Matt Moran <matt.moran@connect-group.com>
2018-07-10 14:38:44 +0100

    Updating library for async methods

--- easyhttp3.js
+++ easyhttp3.js
@@ -2,68 +2,61 @@
  * EasyHTTP Library
  * A Library for making HTTP requests
  * 
- * @version 2.0.0
+ * @version 3.0.0
  * @author Matt Moran
  * @license MIT
  * 
  */
 
- class EasyHTTP {
+class EasyHTTP {
     // Make an HTTP GET Request 
-    get(url) {
-        return new Promise((resolve, reject) => {
-            fetch(url)
-            .then(res => res.json())
-            .then(data => resolve(data))
-            .catch(err => reject(err));
-        });
-     }
+    async get(url) {
+        const response = await fetch(url);
+        const resData = await response.json();
+        return resData;
+    }
 
-     // Make an HTTP POST Request 
-    post(url, data) {
-        return new Promise((resolve, reject) => {
+    // Make an HTTP POST Request 
+    async post(url, data) {
+        const response = await
             fetch(url, {
                 method: 'POST',
                 headers: {
                     'Content-type': 'application/json'
-                }, 
+                },
                 body: JSON.stringify(data)
-            })
-            .then(res => res.json())
-            .then(data => resolve(data))
-            .catch(err => reject(err));
-        });
-     }
+            });
+        const resData = await response.json();
+        return resData;
+    }
+
 
     // Make an HTTP PUT Request 
-     put(url, data) {
-        return new Promise((resolve, reject) => {
+    async put(url, data) {
+        const response = await
             fetch(url, {
                 method: 'PUT',
                 headers: {
                     'Content-type': 'application/json'
-                }, 
+                },
                 body: JSON.stringify(data)
-            })
-            .then(res => res.json())
-            .then(data => resolve(data))
-            .catch(err => reject(err));
-        });
-     }
+            });
 
-     // Make an HTTP DELETE Request
-     delete(url) {
-        return new Promise((resolve, reject) => {
+        const resData = await response.json();
+        return resData;
+    }
+
+    // Make an HTTP DELETE Request
+    async delete(url) {
+        const response = await
             fetch(url, {
                 method: 'DELETE',
                 headers: {
                     'Content-type': 'application/json'
                 }
-            })
-            .then(res => res.json())
-            .then(() => resolve('Resource Deleted...'))
-            .catch(err => reject(err));
-        });
-     }
+            });
+        const resData = await 'Resource deleted... ';
+        return resData;
+    }
 
- }
+}
